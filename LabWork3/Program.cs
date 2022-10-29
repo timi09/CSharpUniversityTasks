156779798de4ff707b3915d89c8a815914e9c607
Евгений Тихонов <etikhonov@cleverence.local>
2022-10-29 14:45:12 +0300

    Add lab 3

---
+++ LabWork3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v3
+{
+    class Program
+    {
+        public static Time Tm1;
+        public static Time Tm2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите время");
+
+            Console.Write("Введите часы: ");
+            string h = Console.ReadLine();
+            Console.Write("Введите минуты: ");
+            string m = Console.ReadLine();
+            Console.Write("Введите секунды: ");
+            string s = Console.ReadLine();
+            Tm1 = new Time(int.Parse(h), int.Parse(m), int.Parse(s));
+            Console.WriteLine(Tm1.ToString());
+
+            Console.WriteLine("Введите вычитаемое время");
+
+            Console.Write("Введите часы: ");
+            h = Console.ReadLine();
+            Console.Write("Введите минуты: ");
+            m = Console.ReadLine();
+            Console.Write("Введите секунды: ");
+            s = Console.ReadLine();
+            Tm2 = new Time(int.Parse(h), int.Parse(m), int.Parse(s));
+            Console.WriteLine(Tm2.ToString());
+
+            Console.WriteLine($"Разность времени: {Tm1 - Tm2}");
+
+            Console.WriteLine("Введите время для умножения");
+
+            Console.Write("Введите часы: ");
+            h = Console.ReadLine();
+            Console.Write("Введите минуты: ");
+            m = Console.ReadLine();
+            Console.Write("Введите секунды: ");
+            s = Console.ReadLine();
+            Tm2 = new Time(int.Parse(h), int.Parse(m), int.Parse(s));
+            Console.WriteLine(Tm2.ToString());
+
+            Console.Write("Введите коэффициент: ");
+            double k = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Умноженное время: {k * Tm2}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LabWork3/Time.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v3
+{
+    public class Time
+    {
+        private int hr, mn, sc;
+        public Time()
+        {
+            hr = 0;
+            mn = 0;
+            sc = 0;
+        }
+        public Time(int h, int m, int s)
+        {
+            hr = h;
+            mn = m;
+            sc = s;
+        }
+        public override string ToString()
+        {
+            return hr.ToString() + ":" + mn.ToString() + ":" + sc.ToString();
+        }
+
+        public static Time operator -(Time leftTime, Time rightTime) 
+        {
+            int leftTotalSec = GetTotalSeconds(leftTime);
+
+            int rightTotalSec = GetTotalSeconds(rightTime);
+
+            int totalSec = leftTotalSec - rightTotalSec;
+
+            Time time = GetTimeFromSeconds(totalSec);
+
+            return time;
+        }
+
+        public static Time operator *(Time time, double multiplier)
+        {
+            int totalSec = GetTotalSeconds(time);
+
+            Time newTime = GetTimeFromSeconds((int)(totalSec * multiplier));
+
+            return newTime;
+        }
+
+        public static Time operator *(double multiplier, Time time)
+        {
+            return time * multiplier;
+        }
+
+        private static int GetTotalSeconds(Time time) 
+        {
+            return time.hr * 3600 + time.mn * 60 + time.sc;
+        }
+
+        private static Time GetTimeFromSeconds(int seconds)
+        {
+            Time time = new Time();
+
+            time.hr = seconds / 3600;
+
+            seconds -= time.hr * 3600;
+
+            time.mn = seconds / 60;
+
+            seconds -= time.mn * 60;
+
+            time.sc = seconds;
+
+            return time;
+        }
+    }
+}
